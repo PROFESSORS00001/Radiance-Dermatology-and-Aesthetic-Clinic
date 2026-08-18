24ec91a1b7e682584bac6615aa60a2559454917c
Alfred <alfrednat2020@gmail.com>
2026-08-18 03:38:01 +0000

    Fix email_service require

--- backend/src/routes/consultations.js
+++ backend/src/routes/consultations.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../firebase');
 const { resolvePatientAndDoctorNames } = require('../resolver');
-const { sendEmail } = require('../email_service');
+const { sendEmail } = require('../mailer');
 
 router.get('/', async (req, res) => {
   try {
